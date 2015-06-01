fd439dde7f97d0ca80e35ed15efd96200a64f49d
J.K <jiukunz@gmail.com>
2015-06-01 23:51:06 +0800

    add felix example 1&2

---
+++ main/src/main/java/com/thoughtworks/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.thoughtworks;
+
+/**
+ * Created by jkzhang on 6/1/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
